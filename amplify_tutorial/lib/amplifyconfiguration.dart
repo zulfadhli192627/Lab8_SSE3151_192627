c6fbc4b9074c97d9b5b101a148d4caedf89d407b
zulfadhli192627 <zul.joey99@gmail.com>
2020-12-29 17:45:44 +0800

    Part 1 & Part 2

---
+++ amplify_tutorial/lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:9491bec4-d559-4e5b-bd91-44f530c15392",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_BrPgRl5sd",
+                        "AppClientId": "723ekjpin2da88u2tqphiit0hn",
+                        "AppClientSecret": "1soel9l06sgd0oacs4k3v6bi343qbdsmactek7i52q8172nt49l9",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
---
+++ amplify_tutorial/lib/main.dart
@@ -0,0 +1,129 @@
+import 'package:amplify_core/amplify_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+
+import 'amplifyconfiguration.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+    routes: {},
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // gives our app awareness about whether we are succesfully connected to the cloud
+  bool _amplifyConfigured = false;
+
+  // Instantiate Amplify
+  Amplify amplifyInstance = Amplify();
+
+  // controllers for text input
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  bool isSignUpComplete = false;
+  bool isSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // amplify is configured on startup
+    _configureAmplify();
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    emailController.dispose();
+    passwordController.dispose();
+
+    super.dispose();
+  }
+
+  void _configureAmplify() async {
+    if (!mounted) return;
+
+    try {
+      AmplifyAuthCognito authPlugin = AmplifyAuthCognito();
+      amplifyInstance.addPlugin(authPlugins: [authPlugin]);
+
+      await amplifyInstance.configure(amplifyconfig);
+      setState(() {
+        _amplifyConfigured = true;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<String> _registerUser(LoginData data) async {
+    try {
+      Map<String, dynamic> userAttributes = {
+        "email": data.name,
+      };
+      SignUpResult res = await Amplify.Auth.signUp(
+          username: data.name,
+          password: data.password,
+          options: CognitoSignUpOptions(userAttributes: userAttributes));
+      setState(() {
+        isSignUpComplete = res.isSignUpComplete;
+        print("Sign up: " + (isSignUpComplete ? "Complete" : "Not Complete"));
+      });
+    } on AuthError catch (e) {
+      print(e);
+      return "Register Error: " + e.toString();
+    }
+  }
+
+  Future<String> _signIn(LoginData data) async {
+    try {
+      SignInResult res = await Amplify.Auth.signIn(
+        username: data.name,
+        password: data.password,
+      );
+      setState(() {
+        isSignedIn = res.isSignedIn;
+      });
+
+      if (res.isSignedIn)
+        Alert(
+                context: context,
+                type: AlertType.success,
+                title: 'Login Success',
+                desc: 'Good Job')
+            .show();
+    } on AuthError catch (e) {
+      Alert(
+          context: context,
+          type: AlertType.error,
+          title: 'Login Failed',
+          desc: e.toString());
+
+      print(e);
+      return 'Log In Error: ' + e.toString();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: FlutterLogin(
+          logo: 'assets/vennify_media.png',
+          onLogin: _signIn,
+          onSignup: _registerUser,
+          onRecoverPassword: (_) => null,
+          title: 'Flutter Amplify'),
+    );
+  }
+}
